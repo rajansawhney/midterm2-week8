0cb47d34e6c795794c093e14ed599c96b657ac77
rajansawhney <rajan.k.sawhney@gmail.com>
2023-10-10 12:19:51 -1000

    remove double comment block

--- banking_system.c
+++ banking_system.c
@@ -21,26 +21,3 @@ Note:
 - Display balance with only two decimal places.
 
 */
-/*
-II. Simple Banking System (65 points)
-
-Write a C program that simulates a simple banking system. The program should allow users to perform the following operations:
-1. Check balance
-2. Deposit funds
-3. Withdraw funds
-4. Exit
-
-Requirements:
-1. Create a function for each operation (Check balance, Deposit, Withdraw).
-2. Implement a function for displaying the menu and getting the user's choice.
-3. Use a switch-case statement to perform the selected operation.
-4. Keep track of the user's balance in a variable.
-5. Ensure that withdrawals cannot exceed the available balance.
-6. Display the balance after each operation.
-Hint: You can create functions for checking the balance, depositing funds, and withdrawing funds, each updating the balance accordingly.
-Use a switch-case statement to call the appropriate operation function based on the user's choice.
-
-Note:
-- Display balance with only two decimal places.
-
-*/
