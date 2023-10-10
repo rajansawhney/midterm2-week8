22f8a46c90fcc17090957fa0882f36ca2eab232d
rajansawhney <rajan.k.sawhney@gmail.com>
2023-10-10 11:13:37 -1000

    midterm2 setup

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # ICS 212
-## Midterm1- Week 8
+## Midterm2 - Week 8
 
 ### Instructions:
-- Update the program files following instructions provided in Midterm_Week8.pdf (uploaded on Laulima) and in the program comments.
+- Update the program files following instructions provided in MidTerm_Wednesday_October_11th_Week8.pdf (uploaded on Laulima) and in the program comments.
 
 **NOTE**: 
 - Github is not a place to test your code. Test it on your machines first. Do not exceed 10 commits. If you exceed 10 commits, your grade will be penalized.
---
+++ fahrenheit_to_celsius.c
@@ -0,0 +1,20 @@
+/*
+I. Degrees Fahrenheit to Celsius (35 points)
+
+Write a C program that prints a table with temperature in degrees Fahrenheit and their corresponding temperature
+ in degrees celsius using the following formula to convert temperatures from degrees Fahrenheit to degrees Celsius:
+        °C = (5⁄9) ∗ (°F − 32)
+
+The table of Fahrenheit temperatures will start at 1 degree then 20 and increment the following temperatures
+ by 20 degrees up to 300 degrees as shown below:
+
+1 -17 
+20 -6
+40 4
+.
+.
+.
+280 137
+300 148
+
+*/
